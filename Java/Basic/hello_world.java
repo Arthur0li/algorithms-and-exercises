d4908fd3c2bba94ad68d324d4a7370c422410d07
Arthur <artholi321@gmail.com>
2025-09-10 17:58:27 -0300

    Add hello world example in Java
    Introduces a basic Java program that prints 'Hello World!' and 'Olá mundo!' to the console. This serves as a simple example for beginners.

---
+++ Java/Basic/hello_world.java
@@ -0,0 +1,8 @@
+package Java.Basic;
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.print("\nHello World! :)\n");
+        System.out.print("\nOlá mundo! :)\n");
+    }    
+}
