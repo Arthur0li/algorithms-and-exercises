351299675fd07287c34bcfe8508850e444c0b99b
Arthur <artholi321@gmail.com>
2025-09-12 10:23:36 -0300

    Add method to hello_world.java and update build configs

--- Java/Basic/hello_world.java
+++ Java/Basic/hello_world.java
@@ -1,8 +1,14 @@
 package Java.Basic;
 
 public class hello_world {
+    public static void method(){
+        System.out.print("\nOlá de dentro do método!\n");
+        System.out.print("\nHello from within the method!\n");
+    }    
+
     public static void main(String[] args) {
         System.out.print("\nHello World! :)\n");
         System.out.print("\nOlá mundo! :)\n");
+        method();
     }    
 }
