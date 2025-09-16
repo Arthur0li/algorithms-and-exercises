a953cf052b3eb1fca7af0cd868d7b18480975e17
Arthur <artholi321@gmail.com>
2025-09-16 20:29:54 -0300

    Add data_types example and remove hello_world class
    Added a new Java example demonstrating basic data types in data_types.java and its compiled class file. Removed the compiled hello_world.class file.

---
+++ Java/Basic/data_types.java
@@ -0,0 +1,17 @@
+package Java.Basic;
+
+public class data_types {
+    public static void main(String[] args) {
+        int number = 0;
+        double number2 = 1.5;
+        char character = 'x';
+        String name = "Arthur";
+
+        System.out.print("\n");
+        System.out.print("This is a variable of a int type: "+number+"\n");
+        System.out.print("This is a variable of a double type: "+number2+"\n");
+        System.out.print("This is a variable of a char type: "+character+"\n");
+        System.out.print("This is a variable of a string type: "+name+"\n");
+        System.out.print("\n");
+    }
+}
