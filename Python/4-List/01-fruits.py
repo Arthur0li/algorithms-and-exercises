8a987f450ef7396bfed7f027d3b2471d08392c6d
Arthur <artholi321@gmail.com>
2025-09-16 16:45:44 -0300

    Add basic list operations example for fruits
    Introduces a Python script demonstrating list indexing, element assignment, appending, and popping using a list of fruits.

---
+++ Python/4-List/01-fruits.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "melon", "watermelon", "pear"]
+print (fruits[0])
+print (fruits[4])
+
+fruits[2] = "cherry"
+
+fruits.append("cherry")
+print (fruits)
+
+fruits.pop(0)
+print (fruits)
