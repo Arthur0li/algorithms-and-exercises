333ce91ace2d44aa12f74f54cf54e7a36527882c
Arthur <artholi321@gmail.com>
2025-09-30 14:27:19 -0300

    Add expense calculator and tuple examples; remove fruit list
    Added a Java expense calculator starter and two Python tuple example files. Removed the Python fruit list script.

---
+++ Java/Basic/expense_calculator.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class expense_calculator {
+    //criar um algoritmo onde o usuario digita a renda mensal e o gasto mensal, depois calcular o que sobrará para o usuario
+
+    public static void main(String[] args) {
+        int renda_mensal = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua renda mensal:"));
+    }
+}
---
+++ Python/4-Tuple-List/Tuple/Test.py
@@ -0,0 +1,6 @@
+lanche = ('hamburguer', 'pizza', 'refrigerante')
+
+print (lanche[2])
+
+for i in lanche:
+    print("Esse lanche é o",i)
