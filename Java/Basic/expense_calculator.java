816db4ab43b9884936e066cbc1b21cb05ada617f
Arthur <artholi321@gmail.com>
2025-10-01 09:41:56 -0300

    Update expense calculator and fix typo in operation
    Changed variable types in expense_calculator.java to float and added calculation and output for remaining balance. Fixed typo in operation.java input prompt ('firs' to 'first'). Removed outdated .class files.

--- Java/Basic/expense_calculator.java
+++ Java/Basic/expense_calculator.java
@@ -4,6 +4,11 @@ public class expense_calculator {
     //criar um algoritmo onde o usuario digita a renda mensal e o gasto mensal, depois calcular o que sobrará para o usuario
 
     public static void main(String[] args) {
-        int renda_mensal = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua renda mensal:"));
+        float renda_mensal = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua renda mensal:"));
+        float gasto_mensal = Float.parseFloat(JOptionPane.showInputDialog("Digite a sua gasto mensal"));
+
+        float resultado = renda_mensal - gasto_mensal;
+
+        JOptionPane.showMessageDialog(null,"Aqui está o que vai sobrar no final do mês: " + resultado);
     }
 }
--- Java/Basic/operation.java
+++ Java/Basic/operation.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class operation {
     public static void main(String[] args) {
-        double number1 = Double.parseDouble(JOptionPane.showInputDialog( "Type the firs number:"));
+        double number1 = Double.parseDouble(JOptionPane.showInputDialog( "Type the first number:"));
         double number2 = Double.parseDouble(JOptionPane.showInputDialog("Type the second number:"));
 
         double sum = number1 + number2;
