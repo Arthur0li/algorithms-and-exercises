86d28510263b504f65cac7916bf48e523863c4f9
Arthur <artholi321@gmail.com>
2025-10-02 15:06:14 -0300

    Add number-to-word converter with input validation
    Introduces a script that converts integers between 0 and 20 to their word representation. Includes input validation and an option to rerun or exit the program.

---
+++ Python/4-Tuple-List/Tuple/01-extended_number.py
@@ -0,0 +1,29 @@
+while True:
+    extended_numbers = ('Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
+                        'Eleven','Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen',
+                        'Eighteen', 'Nineteen', 'Twenty')
+
+    while True:
+        try:
+            number = int(input("Enter an integer between 0 and 20: "))
+            if number < 0 or number > 20:
+                print("Error, you entered a number outside the allowed range.")
+                continue
+            break
+
+        except ValueError:
+            print("You did not enter a number, what a pity :(")
+
+    print("The number you entered was:", extended_numbers[number])
+
+    while True:
+        choice = input("Do you want to run the code again? (Enter y or n) ").lower()
+
+        if choice == 'y':
+            print("You chose to restart the program!\n")
+            break
+        elif choice == 'n':
+            print("You chose to end the program! Thank you for using it :)")
+            exit()
+        else:
+            print("You did not enter y or n :(")
