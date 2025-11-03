0e23658c8f58d2c72291c551a101ff4ec193c5ea
Arthur <artholi321@gmail.com>
2025-11-03 20:48:46 -0300

    Update 01-book.py

--- Python/5-Dict/01-book.py
+++ Python/5-Dict/01-book.py
@@ -1,14 +1,17 @@
-dict_book = {"Título": "", "Autor": "", "Ano": "", "Páginas": ""}
+dict_book = {"Title": "", "Author": "", "Year": "", "Pages": ""}
 
-dict_book["Título"] = input("Digite um título: ")
-dict_book["Autor"] = input("Digite um autor: ")
-dict_book["Ano"] = input("Digite um ano: ")
-dict_book["Páginas"] = input("Digite as páginas do Livro: ")
+dict_book["Title"] = input("Enter a title: ")
+dict_book["Author"] = input("Enter an author: ")
+dict_book["Year"] = input("Enter a year: ")
+dict_book["Pages"] = input("Enter the number of pages: ")
 
-print("\nValores do dicionario", dict_book.values())
 print("Keys do dicionario",dict_book.keys())
 print("Itens do dicionario", dict_book.items(), "\n")
+print("\nDictionary values:", dict_book.values())
+print("Dictionary keys:", dict_book.keys())
+print("Dictionary items:", dict_book.items(), "\n")
 
 print("Organizado:")
-for k,v in dict_book.items():
+print("Organized:")
+for k, v in dict_book.items():
     print(k, "→", v)
