07d53cc50821fb4f7eb39101b3dcdf4c459aadb9
Arthur <artholi321@gmail.com>
2025-11-04 17:46:06 -0300

    Add input validation and restart option to book dict
    Wrapped the book dictionary input process in a loop, added input validation for 'Year' to ensure it is an integer, and introduced an option for the user to restart or exit the program after each entry.

--- Python/5-Dict/01-book.py
+++ Python/5-Dict/01-book.py
@@ -1,17 +1,37 @@
-dict_book = {"Title": "", "Author": "", "Year": "", "Pages": ""}
+while True:
+    dict_book = {"Title": "", "Author": "", "Year": "", "Pages": ""}
 
-dict_book["Title"] = input("Enter a title: ")
-dict_book["Author"] = input("Enter an author: ")
-dict_book["Year"] = input("Enter a year: ")
-dict_book["Pages"] = input("Enter the number of pages: ")
+    dict_book["Title"] = input("Enter a title: ")
+    dict_book["Author"] = input("Enter an author: ")
+    while True:
+        try:
+            dict_book["Year"] = int(input("Enter a year: "))
+            break
+        except ValueError:
+            print("You typed it wrong, you need to enter a number")
+            
+    while True:
+        try:
+            dict_book["Pages"] = input("Enter the number of pages: ")
+            break
+        except ValueError:
+            print("You typed it wrong, you need to enter a number")
 
-print("Keys do dicionario",dict_book.keys())
-print("Itens do dicionario", dict_book.items(), "\n")
-print("\nDictionary values:", dict_book.values())
-print("Dictionary keys:", dict_book.keys())
-print("Dictionary items:", dict_book.items(), "\n")
+    print("\nDictionary values:", dict_book.values())
+    print("Dictionary keys:", dict_book.keys())
+    print("Dictionary items:", dict_book.items(), "\n")
 
-print("Organizado:")
-print("Organized:")
-for k, v in dict_book.items():
-    print(k, "→", v)
+    print("Organized:")
+    for k, v in dict_book.items():
+        print(k, "→", v)
+
+    while True:
+        choice = input("\nDo you want to restart the program? (y/n): ")
+        if choice == 'y':
+            print("You chose to restart the program :)")
+            break
+        elif choice == 'n':
+            print("Bye Bye :)")
+            exit()
+        else:
+            print("You typed it wrong :(")
