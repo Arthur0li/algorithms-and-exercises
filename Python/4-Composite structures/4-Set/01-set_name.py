e3ce93b8d9b5a28c7028798fa63697130acd196a
Arthur <artholi321@gmail.com>
2025-11-10 11:30:28 -0300

    Add interactive set-based name collection script
    Introduces a Python script that allows users to input a specified number of names, stores them in a set to avoid duplicates, and provides an option to restart or exit the program.

---
+++ Python/4-Composite structures/4-Set/01-set_name.py
@@ -0,0 +1,31 @@
+while True:
+    names_set = set()
+    print("\nWelcome to the name-adding program using a set")
+    while True:
+        try:
+            quantity = int(input("Enter the number of names you want to write: "))
+            if quantity > 0:
+                print("You chose", quantity, "times!\n")
+                break
+            else:
+                print("You entered a number less than or equal to zero, try again!\n")
+        except ValueError:
+            print("You did not enter an integer! Try again!\n")
+
+    print("Now it's time to add the names to the set! (Remember that duplicate names will not appear.)")
+    for i in range(quantity):
+        name = input("Enter a name: ")
+        names_set.add(name)
+
+    print("\nHere are the names you added!\n", names_set)
+
+    while True:
+        choice = input("\nDo you want to restart the program? (y/n): ")
+        if choice == "y":
+            print("You chose to restart the program!")
+            break
+        elif choice == "n":
+            print("You chose to end the program, bye! :)")
+            exit()
+        else:
+            print("You entered it incorrectly, try again!")
