b54357d3ffe99fa621b5ffa7581a11b24621567b
Arthur <artholi321@gmail.com>
2026-01-15 12:59:18 -0300

    Add traffic light exercise Arduino sketches
    Added four new Arduino sketches for traffic light exercises: 01-exercise.ino, 02-exercise.ino, 03-exercises.ino, and test.ino. These files demonstrate different traffic light control scenarios using digital outputs and delays.

---
+++ Arduino/03-traffic_light/Exercises/01-exercise.ino
@@ -0,0 +1,51 @@
+int red1 = 1;  
+int yellow1 = 2;
+int green1 = 3; 
+int red2 = 11;  
+int yellow2 = 12;
+int green2= 13;  
+
+void setup() {
+    pinMode(red1, OUTPUT);
+    pinMode(yellow1, OUTPUT);
+    pinMode(green1, OUTPUT);
+    pinMode(red2, OUTPUT);
+    pinMode(yellow2, OUTPUT);
+    pinMode(green2, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(green1,HIGH);
+    digitalWrite(green2,HIGH); 
+    digitalWrite(yellow1,LOW);
+    digitalWrite(yellow2,LOW);
+  	digitalWrite(red1,LOW);
+  	digitalWrite(red2,LOW);
+
+    delay(1000); 
+    
+    digitalWrite(green1,LOW);
+    digitalWrite(green2,LOW);
+ 	digitalWrite(yellow1,HIGH);
+  	digitalWrite(yellow2,HIGH);
+    digitalWrite(red1,LOW);
+  	digitalWrite(red2,LOW);
+    
+    delay(1000);
+  
+    digitalWrite(green1,LOW);
+    digitalWrite(green2,LOW);
+    digitalWrite(yellow1,LOW);
+    digitalWrite(yellow2,LOW);
+ 	digitalWrite(red1,HIGH);
+  	digitalWrite(red2,HIGH);
+  
+  	delay(1000);
+  	
+    digitalWrite(green1,LOW);
+    digitalWrite(green2,LOW);
+    digitalWrite(yellow1,LOW);
+    digitalWrite(yellow2,LOW);
+  	digitalWrite(red1,LOW);
+  	digitalWrite(red2,LOW);
+ }
