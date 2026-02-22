f5613cdd4da15ada278d22ded44b8d0af5a6a0a9
Arthur <artholi321@gmail.com>
2026-02-22 18:53:31 -0300

    Test3

---
+++ C/1-Variables/01-hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+    printf("Olá, mundo!\n");
+    printf("Isso é um teste de 'printf'...\n");
+    printf("Hello World\n");
+    printf("This is a test about the 'printf'...\n");
+    return 0;
+}
