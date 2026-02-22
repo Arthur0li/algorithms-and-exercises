3a26aeb89ff4562153105c7d4bc07852836939d9
Arthur <artholi321@gmail.com>
2026-02-22 18:19:16 -0300

    Test

--- C/01-hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void) {
-    printf("Olá, mundo!\n");
-    printf("Isso é um teste de 'printf'...\n");
-    printf("Hello World\n");
-    printf("This is a test about the 'printf'...\n");
-    return 0;
-}
