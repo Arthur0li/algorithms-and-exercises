c10264a4f153ef31f114a2b2aae0dcdf6f0dbdf9
Arthur <artholi321@gmail.com>
2026-02-22 23:10:25 -0300

    Adding a new code

---
+++ C/1-Variables/03-smallest_multiple_above_a_limit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main(void){
+    float number;
+    float limit;
+    float result;
+
+    printf("Welcome to the code!\n");
+    printf("This code will determine the smallest multiple of an integer that is greater than a specified minimum limit.\n\n");
+    printf("To start, type a number: ");
+    scanf("%f",&number);
+    printf("\nNow type a number that will be the limit: ");
+    scanf("%f",&limit);
+
+    result = floor(limit/number);
+    result = (result+1)*number;
+
+    printf("%f",result);
+}
