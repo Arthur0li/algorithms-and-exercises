8e97ce724914b27e271ad1f3b195fe56af7f6a4c
Arthur <artholi321@gmail.com>
2026-02-22 21:06:30 -0300

    Adding an exercise

---
+++ C/1-Variables/02-change_variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void){
+    int number1;
+    int number2;
+
+    printf("Type the first number: ");
+    scanf("%d",&number1);
+    printf("\nType the second number: ");
+    scanf("%d",&number2);
+
+    number1 = number1 + number2;
+    number2 = number1 - number2;
+    number1 = number1 - number2;
+
+    printf("\nNow the first number is: %d",number1);
+    printf("\nAnd now the second number is: %d",number2);
+}
