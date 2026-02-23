6b19f4bc5f359af2298b60028dee3cfbbbb25d78
Arthur <artholi321@gmail.com>
2026-02-23 15:05:24 -0300

    Adding an exercise

---
+++ C/1-Variables/04-seconds_conversion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void){
+    int total_seconds;
+    int rest;
+    int hours;
+    int minutes;
+    int seconds;
+
+    printf("Welcome to the second conversion code!!\n");
+    printf("This code will determine a time based on the seconds entered by the user.\n\n");
+    printf("To start, type the seconds: ");
+    scanf("%d", total_seconds);
+
+    hours = total_seconds/3600;
+    rest = total_seconds % 3600;
+    minutes = rest / 60;
+    seconds = rest % 60;
+
+
+    printf("The final total_seconds is %dh %dmin %ds",hours,minutes,seconds);
+}
