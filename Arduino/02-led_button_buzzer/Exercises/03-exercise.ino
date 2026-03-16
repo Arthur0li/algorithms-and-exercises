dad84e4a8dc046339b0b1eebe5240c656d1af06a
Arthur <artholi321@gmail.com>
2026-03-16 20:14:03 -0300

    adding comments in the third file of led-button-buzzer exercises

--- Arduino/02-led_button_buzzer/Exercises/03-exercise.ino
+++ Arduino/02-led_button_buzzer/Exercises/03-exercise.ino
@@ -1,3 +1,4 @@
+// pins of the buttons, LEDs and buzzer
 int button_yellow_Pin = 10;
 int led_yellow_Pin = 11;
 int button_blue_Pin = 9;
@@ -7,28 +8,36 @@ int buzzerPin = 8;
 int button_yellow_State = 0;
 int button_blue_State = 0;
 
+// this function setups runs once when the program starts
 void setup() {
+  // set the buttons pins as input and the LEDs and buzzer pins as output
   pinMode(led_yellow_Pin, OUTPUT);
   pinMode(led_blue_Pin, OUTPUT);
   pinMode(button_yellow_Pin, INPUT);
   pinMode(button_blue_Pin, INPUT);
   pinMode(buzzerPin, OUTPUT);
+  // initialize serial communication at 9600 bits per second
   Serial.begin(9600);
 }
 
+// function that repeats indefinitely when the arduino is powered on
 void loop() {
+  // read the state of the buttons and print it to the serial monitor
   button_yellow_State = digitalRead(button_yellow_Pin);
   Serial.println(button_yellow_State);
   
+  // read the state of the buttons and print it to the serial monitor
   button_blue_State = digitalRead(button_blue_Pin);
   Serial.println(button_blue_State);
 
+  // if the yellow button is pressed, turn on the yellow LED, otherwise turn it off
   if (button_yellow_State == HIGH) {
     digitalWrite(led_yellow_Pin, LOW);
   } else {
     digitalWrite(led_yellow_Pin, HIGH);
   }
   
+  // if the blue button is pressed, turn on the blue LED and the buzzer, otherwise turn them off
   if (button_blue_State == HIGH) {
     digitalWrite(led_blue_Pin, LOW);
     noTone(buzzerPin);
