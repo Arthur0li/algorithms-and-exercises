1e1dac49dba32b51adc85395a4c7f8e8b2cf1aa9
Arthur <artholi321@gmail.com>
2026-03-16 19:53:21 -0300

    adding comments int the fifth arduino file

--- Arduino/01-led/Exercises/05-exercise.ino
+++ Arduino/01-led/Exercises/05-exercise.ino
@@ -1,19 +1,25 @@
+// pins of the LEDs
 int ledA = 11;
 int ledB = 12;
 int builtinLed = LED_BUILTIN;
 
+// this function setups runs once when the program starts
 void setup(){
+    // set the LEDs pins as output
     pinMode(ledA, OUTPUT);
     pinMode(ledB, OUTPUT);
     pinMode(builtinLed, OUTPUT);
 }
 
+// function that repeats indefinitely when the arduino is powered on
 void loop(){
+    // turn on the LEDs for 2 seconds
     digitalWrite(builtinLed,HIGH);
     digitalWrite(ledA,LOW);
     digitalWrite(ledB,LOW);
     delay(2000);
 
+    // turn on the LEDs for 3 seconds
     digitalWrite(ledA,HIGH);
     digitalWrite(ledB,HIGH);
     digitalWrite(builtinLed,LOW);
