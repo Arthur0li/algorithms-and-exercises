6733949a0b44f2520a9f8ae98ee93ef427217926
Arthur <artholi321@gmail.com>
2026-03-25 16:37:46 -0300

    Adding comments

--- Python/1-Basic/03-successor_number.py
+++ Python/1-Basic/03-successor_number.py
@@ -1,4 +1,6 @@
 '''
+The "input" command allowed displaying a message to the user and receiving the information that the user would type.
+In this file we can see the command being used to display a message and receive a number!
 The input command has a drawback; by default, it returns a string.
 To change this, we must type the variable type before the input.
 
