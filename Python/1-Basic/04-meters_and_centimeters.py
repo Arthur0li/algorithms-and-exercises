ea6972b4cf9b53f746f3cd405b8e3e61c88ea049
Arthur <artholi321@gmail.com>
2026-03-25 16:40:47 -0300

    adding comments to the fourth file in the Python 'Basic' subfolder

--- Python/1-Basic/04-meters_and_centimeters.py
+++ Python/1-Basic/04-meters_and_centimeters.py
@@ -1,3 +1,12 @@
+'''
+The "input" command allowed displaying a message to the user and receiving the information that the user would type.
+In this file we can see the command being used to display a message and receive a number!
+The input command has a drawback; by default, it returns a string.
+To change this, we must type the variable type before the input.
+
+In this code the user will type, the code will convert the number to a float, and after this the code will display the final result of the conversion!
+'''
+
 meters = float(input("Type a meter, please: "))
 
 print("\nThe", meters, "in centimeters is", meters*100)
