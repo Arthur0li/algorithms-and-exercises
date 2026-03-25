db268a5e07f64b10c0ef992560e93ed4603a6341
Arthur <artholi321@gmail.com>
2026-03-25 16:44:05 -0300

    adding comments to the sixth file in the Python 'Basic' subfolder

--- Python/1-Basic/06-days_of_life.py
+++ Python/1-Basic/06-days_of_life.py
@@ -1,3 +1,12 @@
+'''
+The "input" command allowed displaying a message to the user and receiving the information that the user would type.
+In this file we can see the command being used to display a message and receive a number!
+The input command has a drawback; by default, it returns a string.
+To change this, we must type the variable type before the input.
+
+In this code the user will type, the code will convert the number to a integer, and after this the code will display the final result!
+'''
+
 years = int(input("Type the years: "))
 months = int(input("Type the months: "))
 days = int(input("Type days: "))
