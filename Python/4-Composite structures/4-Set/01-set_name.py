ef7313e48f6c2298180077dc46691c3b10d2d23e
Arthur <artholi321@gmail.com>
2026-03-28 15:43:55 -0300

    adding comments to the set subfolder

--- Python/4-Composite structures/4-Set/01-set_name.py
+++ Python/4-Composite structures/4-Set/01-set_name.py
@@ -1,33 +1,56 @@
+# this loop keeps the program running until the user decides to stop.
 while True:
+
+    # create a set to store unique names.
     names_set = set()
+
     print("\nWelcome to the name-adding program using a set")
+
     while True:
         try:
+            # ask how many names the user wants to enter.
             quantity = int(input("Enter the number of names you want to write: "))
+
+            # check if the number is greater than 0.
             if quantity > 0:
                 print("You chose", quantity, "times!\n")
                 break
             else:
                 print("You entered a number less than or equal to zero, try again!\n")
+
         except ValueError:
+            # if the user types something invalid, show an error message.
             print("You did not enter an integer! Try again!\n")
 
     print("Now it's time to add the names to the set! (Remember that duplicate names will not appear.)")
+
+    # collect names from the user.
     for i in range(quantity):
         name = input("Enter a name: ")
+
+        # add the name to the set (duplicates are ignored automatically).
         names_set.add(name)
 
     print("\nHere are the names you added!")
+
+    # display all names in the set.
     for names in names_set:
         print(names)
 
     while True:
+        # ask the user if they want to restart the program.
         choice = input("\nDo you want to restart the program? (y/n): ")
+
+        # if the user types 'y', the program restarts.
         if choice == "y":
             print("You chose to restart the program!")
             break
+
         elif choice == "n":
+            # if the user types 'n', we end the program.
             print("You chose to end the program, bye! :)")
             exit()
+
         else:
+            # if the user types something else, show an error message.
             print("You entered it incorrectly, try again!")
--- Python/4-Composite structures/4-Set/02-set_operations.py
+++ Python/4-Composite structures/4-Set/02-set_operations.py
@@ -1,4 +1,7 @@
+# this loop keeps the program running until the user decides to stop.
 while True:
+
+    # create two sets to store the numbers.
     A = set()
     B = set()
 
@@ -6,59 +9,79 @@ while True:
 
     while True:
         try:
+            # ask how many elements the first set will have.
             quantidade = int(input("Enter the number of elements for the first set: "))
+
+            # check if the number is valid.
             if quantidade > 0:
                 print("You entered", quantidade, "times!\n")
                 break
             else:
                 print("You entered a number less than or equal to zero! Try again!\n")
+
         except ValueError:
-            print("You did not enter an integer! Try again!\n")        
-    
+            # if the user types something invalid, show an error message.
+            print("You did not enter an integer! Try again!\n")
+
     print("Now it's time for you to enter the elements of the first set!")
+
+    # collect elements for set A.
     for i in range(quantidade):
         while True:
             try:
                 number = int(input("Enter a number! "))
+
+                # only accept positive numbers.
                 if number > 0:
                     A.add(number)
                     break
                 else:
                     print("You entered a number less than or equal to zero! Try again!\n")
+
             except ValueError:
                 print("You did not enter an integer!\n")
 
-    print("Here is the first set:", A )
+    print("Here is the first set:", A)
+
     print("\nNow it's time for you to enter the elements of the second set!")
 
     while True:
         try:
+            # ask how many elements the second set will have.
             quantidade = int(input("Enter the number of elements for the second set: "))
+
             if quantidade > 0:
                 print("You entered", quantidade, "times!\n")
                 break
             else:
                 print("You entered a number less than or equal to zero! Try again!\n")
+
         except ValueError:
-            print("You did not enter an integer! Try again!\n")        
-    
+            print("You did not enter an integer! Try again!\n")
+
     print("Now it's time for you to enter the elements of the second set!")
+
+    # collect elements for set B.
     for i in range(quantidade):
         while True:
             try:
                 number = int(input("Enter a number! "))
+
+                # only accept positive numbers.
                 if number > 0:
                     B.add(number)
                     break
                 else:
                     print("You entered a number less than or equal to zero! Try again!\n")
+
             except ValueError:
                 print("You did not enter an integer!\n")
-    
-    print("Here is the second set:", B )
+
+    print("Here is the second set:", B)
 
     print("\nNow let's show the results of the operations!\n")
 
+    # show set operations results.
     print("Here is the union of the sets: ", A | B)
     print("Here is the intersection of the sets: ", A & B)
     print("Here are the elements present in A but not in B: ", A - B)
@@ -66,13 +89,20 @@ while True:
     print("Here is the symmetric difference: ", A ^ B)
 
     while True:
+        # ask the user if they want to exit or restart.
         choice = input("\nDo you want to exit the program? (y/n) ")
+
+        # if the user types 'y', we end the program.
         if choice == "y":
             print("\nYou chose to exit the program!")
             print("Bye! :)")
             exit()
+
         elif choice == "n":
+            # if the user types 'n', we restart the program.
             print("You chose to restart the program!")
             break
+
         else:
+            # if the user types something else, show an error message.
             print("You typed it wrong, please enter again!")
